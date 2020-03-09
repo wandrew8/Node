cbef6079a80caf477b1d1ff5234267751b9e6a2b
Andrew Weiss <wandrew8@gmail.com>
2020-03-09 14:24:30 -0700

    new files added

---
+++ node-example/rectangle.js
@@ -0,0 +1,16 @@
+// export function rect(x, y, callback) {
+//     if (x <= 0 || y <= 0) {
+//             callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
+//     } else {
+//         setTimeout(() =>
+//             callback(null, {
+//                 perimeter: () => 2 * (x + y),
+//                 area: () => x * y
+//             }),
+//             2000
+//         );
+//     }
+// };
+const hello = 'hello';
+
+export default hello;
