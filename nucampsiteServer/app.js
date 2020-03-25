beee99d054f63bd282b09fef3da9c36d23725248
Andrew Weiss <wandrew8@gmail.com>
2020-03-25 14:22:57 -0700

    user authentication added using passport and local-sessions strategy

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -42,25 +44,22 @@ app.use(session({
   store: new FileStore()
 }))
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //User Authentication controls access to files until authenticated
 function auth(req, res, next) {
-  console.log(req.session)
+  console.log(req.user)
 
-  if (!req.session.user) {
+  if (!req.user) {
       const err = new Error('You are not authenticated!');
       err.status = 401;
       return next(err);   
   } else {
-    if (req.session.user === 'authenticated') {
       return next();
-    } else {
-      const err = new Error('You are not autheticated!');
-      err.status = 401;
-      return next(err);
-    }
   }
 }
 
